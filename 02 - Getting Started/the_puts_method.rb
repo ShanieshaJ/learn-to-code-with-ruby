c9129adf071856117405444735c06c65d429cd8c
Shaniesha John <shaniesha.john.94@gmail.com>
2023-08-30 22:55:51 -0400

    Adding these files

---
+++ 02 - Getting Started/the_puts_method.rb
@@ -0,0 +1,15 @@
+puts 1
+puts 1 + 2
+puts (1 + 2)
+3 + 5
+
+puts "Hello World"
+puts "I am learning to code!"
+puts 3.14159
+puts
+puts "I am alive and well!"
+puts 3
+puts 4
+puts 5
+
+puts 3, 4, 5
